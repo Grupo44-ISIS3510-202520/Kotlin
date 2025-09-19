1e990d0acfb90a2cc2c95e9d195654bdcd02c658
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2025-09-18 20:08:33 -0500

    Config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.brigadeapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.brigadeapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
