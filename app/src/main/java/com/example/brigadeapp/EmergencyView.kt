0e78ad831eae28914d21392552324fb6a992a9ac
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2025-09-19 22:27:27 -0500

    EmergencyView

---
+++ app/src/main/java/com/example/brigadeapp/EmergencyView.kt
@@ -0,0 +1,14 @@
+package com.example.brigadeapp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun EmergencyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "dashboard") {
+        composable("dashboard") { EmergencyDashboardScreen(navController) }
+    }
+}
