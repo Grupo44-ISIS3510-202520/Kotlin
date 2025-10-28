88cdc783a9e870d42044fb25b25ff3250ca9d4fa
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2025-10-27 19:45:27 -0500

    Reintegration

--- app/src/main/java/com/example/brigadeapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/brigadeapp/di/NetworkModule.kt
@@ -5,17 +5,28 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    val token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhua2dlanBwZmtvZ3hmem1senF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMjI2NTgsImV4cCI6MjA3Njg5ODY1OH0.iGBLMN1yk_cYpv-m2QBW6RBi1x_xBaDFlPfsjwqbixE"
+    val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
 
     @Provides
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://file-uploader-973554845747.europe-west1.run.app")
+            .baseUrl("https://xnkgejppfkogxfzmlzqu.supabase.co/functions/v1/")
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
